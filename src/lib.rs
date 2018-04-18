f1bd09bef04ea44c3f807b333bdd3b9ab410d8f4
Jeroen Bollen <contact@jeroenbollen.eu>
2018-04-18 12:05:23 +0200

    Added benchmarks for fragmented heaps.

--- src/lib.rs
+++ src/lib.rs
@@ -5,6 +5,7 @@ extern crate test;
 
 use rand::Rng;
 use rand::SeedableRng;
+use std::iter::repeat;
 
 const SIZE: usize = 1 << 20;
 
@@ -17,14 +18,43 @@ fn generate_random_stuff() -> Box<[usize; SIZE]> {
     return array;
 }
 
+fn fragment<T>(mut vector: Vec<T>) -> Vec<T> {
+    let mut rng = rand::StdRng::new().unwrap();
+    let mut permutated_vector = Vec::with_capacity(vector.len());
+    while (!vector.is_empty()) {
+        let vector_len = vector.len();
+        let value = vector.swap_remove(rng.gen_range(0, vector_len));
+        permutated_vector.push(value);
+    }
+    permutated_vector
+}
+
 #[bench]
 fn pointer_bench(bencher: &mut test::Bencher) {
-    let mut vector = Vec::<Box<u64>>::with_capacity(SIZE);
-    let mut rng = rand::StdRng::new().unwrap();
+    let vector: Vec<Box<u64>> = rand::StdRng::new()
+        .unwrap()
+        .gen_iter()
+        .map(Box::new)
+        .take(SIZE)
+        .collect();
 
-    for _ in 0..SIZE {
-        vector.push(Box::new(rng.next_u32() as u64));
-    }
+    bencher.iter(|| {
+        for i in 0..SIZE / 2 {
+            let alpha = test::black_box(*vector[i] + *vector[SIZE - i - 1]);
+        }
+    });
+}
+
+#[bench]
+fn pointer_fragmented_heap_bench(bencher: &mut test::Bencher) {
+    let vector: Vec<Box<u64>> = rand::StdRng::new()
+        .unwrap()
+        .gen_iter()
+        .map(Box::new)
+        .take(SIZE)
+        .collect();
+
+    let vector = fragment(vector);
 
     bencher.iter(|| {
         for i in 0..SIZE / 2 {
@@ -35,12 +65,7 @@ fn pointer_bench(bencher: &mut test::Bencher) {
 
 #[bench]
 fn direct_bench(bencher: &mut test::Bencher) {
-    let mut vector = Vec::<u64>::with_capacity(SIZE);
-    let mut rng = rand::StdRng::new().unwrap();
-
-    for _ in 0..SIZE {
-        vector.push(rng.next_u32() as u64);
-    }
+    let vector: Vec<u64> = rand::StdRng::new().unwrap().gen_iter().take(SIZE).collect();
 
     bencher.iter(|| {
         for i in 0..SIZE / 2 {
@@ -51,12 +76,30 @@ fn direct_bench(bencher: &mut test::Bencher) {
 
 #[bench]
 fn pointer_access_bench(bencher: &mut test::Bencher) {
-    let mut vector = Vec::<Box<u64>>::with_capacity(SIZE);
-    let mut rng = rand::StdRng::new().unwrap();
+    let vector: Vec<Box<u64>> = rand::StdRng::new()
+        .unwrap()
+        .gen_iter()
+        .map(Box::new)
+        .take(SIZE)
+        .collect();
 
-    for _ in 0..SIZE {
-        vector.push(Box::new(rng.next_u32() as u64));
-    }
+    bencher.iter(|| {
+        for i in 0..SIZE {
+            let alpha = test::black_box(*vector[i]);
+        }
+    });
+}
+
+#[bench]
+fn pointer_access_fragmented_heap_bench(bencher: &mut test::Bencher) {
+    let vector: Vec<Box<u64>> = rand::StdRng::new()
+        .unwrap()
+        .gen_iter()
+        .map(Box::new)
+        .take(SIZE)
+        .collect();
+
+    let vector = fragment(vector);
 
     bencher.iter(|| {
         for i in 0..SIZE {
@@ -67,12 +110,7 @@ fn pointer_access_bench(bencher: &mut test::Bencher) {
 
 #[bench]
 fn direct_access_bench(bencher: &mut test::Bencher) {
-    let mut vector = Vec::<u64>::with_capacity(SIZE);
-    let mut rng = rand::StdRng::new().unwrap();
-
-    for _ in 0..SIZE {
-        vector.push(rng.next_u32() as u64);
-    }
+    let vector: Vec<u64> = rand::StdRng::new().unwrap().gen_iter().take(SIZE).collect();
 
     bencher.iter(|| {
         for i in 0..SIZE {
@@ -83,12 +121,12 @@ fn direct_access_bench(bencher: &mut test::Bencher) {
 
 #[bench]
 fn pointer_rand_access_bench(bencher: &mut test::Bencher) {
-    let mut vector = Vec::<Box<u64>>::with_capacity(SIZE);
-    let mut rng = rand::StdRng::new().unwrap();
-
-    for _ in 0..SIZE {
-        vector.push(Box::new(rng.next_u32() as u64));
-    }
+    let vector: Vec<Box<u64>> = rand::StdRng::new()
+        .unwrap()
+        .gen_iter()
+        .map(Box::new)
+        .take(SIZE)
+        .collect();
 
     let index_array = generate_random_stuff();
 
@@ -100,13 +138,27 @@ fn pointer_rand_access_bench(bencher: &mut test::Bencher) {
 }
 
 #[bench]
-fn direct_rand_access_bench(bencher: &mut test::Bencher) {
-    let mut vector = Vec::<u64>::with_capacity(SIZE);
-    let mut rng = rand::StdRng::new().unwrap();
+fn pointer_rand_access_fragmented_heap_bench(bencher: &mut test::Bencher) {
+    let vector: Vec<Box<u64>> = rand::StdRng::new()
+        .unwrap()
+        .gen_iter()
+        .map(Box::new)
+        .take(SIZE)
+        .collect();
 
-    for _ in 0..SIZE {
-        vector.push(rng.next_u32() as u64);
-    }
+    let vector = fragment(vector);
+    let index_array = generate_random_stuff();
+
+    bencher.iter(|| {
+        for i in 0..SIZE {
+            let alpha = test::black_box(*vector[index_array[i]]);
+        }
+    });
+}
+
+#[bench]
+fn direct_rand_access_bench(bencher: &mut test::Bencher) {
+    let vector: Vec<u64> = rand::StdRng::new().unwrap().gen_iter().take(SIZE).collect();
 
     let index_array = generate_random_stuff();
 
